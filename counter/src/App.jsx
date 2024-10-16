f6e15f0571c0ec8c26941751a767da15a947df83
ShoryaD <shoryadubey7610@gmail.com>
2024-10-16 06:14:29 +0530

    counter updated

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,92 +1,98 @@
-// import { useState } from 'react'
-// import './App.css'
-
-// function App() {
-
-//   let [counter, setCounter] = useState(0)
-
-//   const updateValue = () => {
-//     // counter = counter + 1
-//     if (counter < 20) {
-//       setCounter(counter + 1)
-//     }
-//   }
-
-//   const deleteValue = () => {
-//     if (counter > 0) {
-//       setCounter(counter - 1);
-//     }
-//   }
-
-//   return (
-//     <>
-//      <h1>Counter : {counter}</h1>
-//      <button onClick={updateValue} style={{marginRight: '10px'}}>Add Value</button>
-//      <button onClick={deleteValue}>Remove Value</button>
-//     </>
-//   )
-// }
-
-// export default App
-
-import { useState, useRef } from 'react';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
 function App() {
-  let [counter, setCounter] = useState(0);
-  const intervalRef = useRef(null);
 
-  const increment = () => {
-    setCounter(prevCounter => prevCounter + 1);
-  };
+  let [counter, setCounter] = useState(0)
 
-  const decrement = () => {
-    setCounter(prevCounter => (prevCounter > 0 ? prevCounter - 1 : 0));
-  };
-
-  const startIncrement = () => {
-    if (!intervalRef.current) {
-      intervalRef.current = setInterval(() => {
-        setCounter(prevCounter => prevCounter + 1);
-      }, 100);
+  const updateValue = () => {
+    // counter = counter + 1
+    if (counter < 20) {
+      setCounter((prevCounter) => prevCounter + 1)
+      setCounter((prevCounter) => prevCounter + 1)
+      setCounter((prevCounter) => prevCounter + 1)
+      setCounter((prevCounter) => prevCounter + 1)
     }
-  };
+  }
 
-  const startDecrement = () => {
-    if (!intervalRef.current) {
-      intervalRef.current = setInterval(() => {
-        setCounter(prevCounter => (prevCounter > 0 ? prevCounter - 1 : 0));
-      }, 100);
+  const deleteValue = () => {
+    if (counter > 0) {
+      setCounter((prevCounter) => prevCounter - 1)
+      setCounter((prevCounter) => prevCounter - 1)
+      setCounter((prevCounter) => prevCounter - 1)
+      setCounter((prevCounter) => prevCounter - 1)
     }
-  };
-
-  const stopPress = () => {
-    if (intervalRef.current) {
-      clearInterval(intervalRef.current);
-      intervalRef.current = null;
-    }
-  };
+  }
 
   return (
     <>
-      <h1>Counter : {counter}</h1>
-      <button 
-        onClick={increment}
-        onMouseDown={startIncrement} 
-        onMouseUp={stopPress} 
-        onMouseLeave={stopPress}
-        style={{ marginRight: '10px' }}>
-        Add Value
-      </button>
-      <button 
-        onClick={decrement}
-        onMouseDown={startDecrement} 
-        onMouseUp={stopPress} 
-        onMouseLeave={stopPress}>
-        Remove Value
-      </button>
+     <h1>Counter : {counter}</h1>
+     <button onClick={updateValue} style={{marginRight: '10px'}}>Add Value</button>
+     <button onClick={deleteValue}>Remove Value</button>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
+
+// import { useState, useRef } from 'react';
+// import './App.css';
+
+// function App() {
+//   counter [counter, setCounter] = useState(0);
+//   const intervalRef = useRef(null);
+
+//   const increment = () => {
+//     setCounter(prevCounter => prevCounter + 1);
+//   };
+
+//   const decrement = () => {
+//     setCounter(prevCounter => (prevCounter > 0 ? prevCounter - 1 : 0));
+//   };
+
+//   const startIncrement = () => {
+//     if (!intervalRef.current) {
+//       intervalRef.current = setInterval(() => {
+//         setCounter(prevCounter => prevCounter + 1);
+//       }, 100);
+//     }
+//   };
+
+//   const startDecrement = () => {
+//     if (!intervalRef.current) {
+//       intervalRef.current = setInterval(() => {
+//         setCounter(prevCounter => (prevCounter > 0 ? prevCounter - 1 : 0));
+//       }, 100);
+//     }
+//   };
+
+//   const stopPress = () => {
+//     if (intervalRef.current) {
+//       clearInterval(intervalRef.current);
+//       intervalRef.current = null;
+//     }
+//   };
+
+//   return (
+//     <>
+//       <h1>Counter : {counter}</h1>
+//       <button 
+//         onClick={increment}
+//         onMouseDown={startIncrement} 
+//         onMouseUp={stopPress} 
+//         onMouseLeave={stopPress}
+//         style={{ marginRight: '10px' }}>
+//         Add Value
+//       </button>
+//       <button 
+//         onClick={decrement}
+//         onMouseDown={startDecrement} 
+//         onMouseUp={stopPress} 
+//         onMouseLeave={stopPress}>
+//         Remove Value
+//       </button>
+//     </>
+//   );
+// }
+
+// export default App;
