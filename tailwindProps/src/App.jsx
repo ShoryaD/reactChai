506230e8ddbefeeddc5b16e87f8d6860c7e37b09
ShoryaD <shoryadubey7610@gmail.com>
2024-10-16 06:03:09 +0530

    tailwindProp done

---
+++ tailwindProps/src/App.jsx
@@ -0,0 +1,15 @@
+import Card from './components/Card'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>Card Component</h1>
+    <Card username="Shorya Dubey" btnText="Click Me"/>
+    <Card username="Om.Shorya"/>
+    </>
+  )
+}
+
+export default App
