e831f9d63ae78c354266b51ea448da4c3c238ad9
ShoryaD <shoryadubey7610@gmail.com>
2024-10-26 23:11:36 +0530

    basic reactReduxToolkitTodo done

---
+++ reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import AddTodo from './components/AddTodo.jsx'
+import Todos from './components/Todos.jsx'
+
+function App() {
+
+  return (
+    <>
+      <h1>React-Redux</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
